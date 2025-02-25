32541c4d53842f481942a387d04967752fd6410c
dillibabu2003 <dillibabu.r2021csea@sece.ac.in>
2025-02-25 21:42:32 +0530

    Add user routes and profile picture upload functionality

---
+++ backend/middlewares/multer.js
@@ -0,0 +1,23 @@
+const multer=require("multer");
+const path=require("path");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/profile-pics')
+    },
+    filename: function (req, file, cb) {
+        const fileName = path.basename(req.user.id+path.extname(file.originalname));
+      if(!fileName){
+        cb(new Error("Failed to Upload image! User Id is required"),null)
+      }else{
+       cb(null,fileName)
+      }
+    }
+  })
+  
+  const profilePicUpload = multer({ storage: storage }
+
+  );
+
+  module.exports={profilePicUpload};
+  
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const cors=require("cors");
+
 const app = express();
 
 const indexRouter = require('../routes/index');
+const bodyParser = require('body-parser');
 
-app.use(express.json({
-    limit: "200kb",
-}));
-app.use(morgan('dev'));
+app.use(cors());
+// app.use(bodyParser.urlencoded())
 app.use(express.urlencoded({
-    extended: true,
+    extended: false,
 }));
+app.use(express.json());
+app.use(morgan('dev'));
 app.use(cookieParser());
 
 app.use("/api/v1",indexRouter);
