b2935f8f69b319fede365eec9bb789a659ccacc0
Golla Pradeep Kumar <pradeepubuntuvps@gmail.com>
2025-03-09 21:36:11 +0530

    Refactor checkout transaction logic; add employee validation function and improve error handling for checkout information

--- backend/controllers/checkout.js
+++ backend/controllers/checkout.js
@@ -20,7 +20,16 @@ async function validateAvailabilityAndReturnItems(model, filters, requiredCount)
     const items = await model.find({...filters}).limit(requiredCount);
     const availableCount = items?.length || 0;
     const result = {areAvailable: availableCount >= requiredCount, items: items};
+    console.log(result);
+    
     return result;
+}
+async function validateEmployeeAndReturnEmployeeDetails(employee_id){
+    const employeeDetails = await Employee.findOne({employee_id: employee_id});
+    if(!employeeDetails){
+        throw new ApiError(400,null,'Employee does not exist');
+    }    
+    return employeeDetails;
 }
 async function executeCheckoutTransaction(model, filters, object_id, employees_info, requiredItemCount){
     const session = await mongoose.startSession();
@@ -31,16 +40,13 @@ async function executeCheckoutTransaction(model, filters, object_id, employees_i
             throw new ApiError(400,null,'Checkout denied due to unavailability');
         }
         const entireCheckoutCountTillNow = await Checkout.countDocuments({},{session: session});
-        for (let i = 0; i < requiredItemCount; i++) {
-            const isEmployeeExists = await Employee.exists({employee_id: employees_info[i].employee_id});
-            if(!isEmployeeExists){
-                throw new ApiError(400,null,'Employee does not exist');
-            }
+        for (let i = 0; i < requiredItemCount; i++) {            
+            const employeeDetails = await validateEmployeeAndReturnEmployeeDetails(employees_info[i].employee_id);
             const totalCheckoutsTillNowOfEmployee = await Checkout.countDocuments({employee_id: employees_info[i].employee_id},{session: session});            
-            const generatedId = items[i].generateId(employees_info[i].employee_id,totalCheckoutsTillNowOfEmployee);
-            items[i].status = "deployed";
-            items[i].assigned_to = employees_info[i]._id;
-            const checkout = await createCheckout(object_id, employees_info[i],items[i]._id,entireCheckoutCountTillNow+i+1);
+            const generatedId = items[i].generateId(employeeDetails.employee_id,totalCheckoutsTillNowOfEmployee);
+            items[i].status = object_id=="assets" ? "deployed" : "activated";
+            items[i].assigned_to = employeeDetails._id;
+            const checkout = await createCheckout(object_id, employeeDetails,items[i]._id,entireCheckoutCountTillNow+i+1);
             await checkout.save({session: session});
             await items[i].save({session: session});
         }
@@ -72,15 +78,16 @@ const handleIndividualCheckout = asyncHandler(async (req, res) => {
 });
 
 const handleBulkCheckout = asyncHandler(async (req, res) => {
-    const {object_id,employees_info,filters,requiredItemCount} = req.body;
-    if(!object_id || !employees_info|| employees_info.length==0 || !filters || typeof filters !== 'object'||filters.status!="available" ||filters.status!="reissue"){
-        throw new ApiError(422,null,'Invalid checkout information');
+    const {object_id,employees_info,filters} = req.body;    
+    if(!object_id || !employees_info|| employees_info.length==0 || !filters || typeof filters !== 'object'||(filters.status!="available" &&filters.status!="reissue")){
+        throw new ApiError(422,null,'Invalid checkout information item type or employee info or filters or required count is missing.'); 
     }
     const model = getModelByObjectId(object_id);
     if(!model){
         throw new ApiError(400,null,'Invalid Checkout');
     }
-    await executeCheckoutTransaction(model, filters, object_id, employees_info,requiredItemCount);
+    
+    await executeCheckoutTransaction(model, filters, object_id, employees_info,employees_info.length);
     res.status(200).json(new ApiResponse(200,null,'Checkout successfull'));
 });
 module.exports = {handleIndividualCheckout, handleBulkCheckout};
