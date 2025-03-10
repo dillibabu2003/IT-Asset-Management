6ddef1a1bd2b44adbf0a514750293f59ca798fbb
dillibabu2003 <dillibabu.r2021csea@sece.ac.in>
2025-03-10 15:08:57 +0530

    Refactor dynamic object routes, add asset funcionality added

--- backend/routes/dynamic_object.js
+++ backend/routes/dynamic_object.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const authorizeClient = require('../middlewares/authorizeClient');
 const dynamicObjectRouter = express.Router();
-const {getPaginatedDataByObjectId,getUserColumnVisibilitiesByObjectId,createBulkDocumentsOfObjectId, createDocumentOfObjectId,getDataBySearchTermOfObjectId, getAllDataByFilterOfObjectId } = require('../controllers/dynamic_object');
 
-dynamicObjectRouter.get("/:objectId",(req,res,next)=>{ authorizeClient([`view:${req.params.objectId}`])(req,res,next)},getPaginatedDataByObjectId);
-dynamicObjectRouter.get('/:objectId/column-visibilities',(req,res,next)=>{authorizeClient([`view:${req.params.objectId}`])(req,res,next)},getUserColumnVisibilitiesByObjectId);
-dynamicObjectRouter.post("/:objectId/bulk",(req,res,next)=>{authorizeClient([`create:${req.params.objectId}`])(req,res,next)},createBulkDocumentsOfObjectId);
-dynamicObjectRouter.post("/:objectId/create",(req,res,next)=>{authorizeClient([`create:${req.params.objectId}`])(req,res,next)},createDocumentOfObjectId);
-dynamicObjectRouter.post("/:objectId/search",(req,res,next)=>{authorizeClient([`view:${req.params.objectId}`])(req,res,next)},getDataBySearchTermOfObjectId);
-dynamicObjectRouter.post("/:objectId/filter-docs/all",(req,res,next)=>{authorizeClient([`view:${req.params.objectId}`])(req,res,next)},getAllDataByFilterOfObjectId);
+const {getPaginatedDataByObjectName,getUserColumnVisibilitiesByObjectName,createBulkDocumentsOfObjectName, createDocumentOfObjectName,getDataBySearchTermOfObjectName, getAllDataByFilterOfObjectName } = require('../controllers/dynamic_object');
+
+dynamicObjectRouter.get("/:objectName",(req,res,next)=>{ authorizeClient([`view:${req.params.objectName}`])(req,res,next)},getPaginatedDataByObjectName);
+dynamicObjectRouter.get('/:objectName/column-visibilities',(req,res,next)=>{authorizeClient([`view:${req.params.objectName}`])(req,res,next)},getUserColumnVisibilitiesByObjectName);
+dynamicObjectRouter.post("/:objectName/bulk",(req,res,next)=>{authorizeClient([`create:${req.params.objectName}`])(req,res,next)},createBulkDocumentsOfObjectName);
+dynamicObjectRouter.post("/:objectName/create",(req,res,next)=>{authorizeClient([`create:${req.params.objectName}`])(req,res,next)},createDocumentOfObjectName);
+dynamicObjectRouter.post("/:objectName/search",(req,res,next)=>{authorizeClient([`view:${req.params.objectName}`])(req,res,next)},getDataBySearchTermOfObjectName);
+dynamicObjectRouter.post("/:objectName/filter-docs/all",(req,res,next)=>{authorizeClient([`view:${req.params.objectName}`])(req,res,next)},getAllDataByFilterOfObjectName);
 module.exports = dynamicObjectRouter;
